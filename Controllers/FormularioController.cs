bdf402f5439862a4b17adc08773e2d3c9a414a53
Pickle <alonsochullunquia@gmail.com>
2023-06-21 15:33:13 -0500

    migración sin estilo a Views/Formulario/Index.cshtml y Controllers/Formulario.cs

---
+++ Controllers/FormularioController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyect_mvc.Controllers
+{
+    public class FormularioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
