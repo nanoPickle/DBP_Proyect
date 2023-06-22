150ad8fa3db88dda18ce5d8cb0f63195bc804691
Pickle <alonsochullunquia@gmail.com>
2023-06-22 10:16:54 -0500

    Elección de ciudades hecha, Recibir y entregar info de input-text hecha

--- Controllers/FormularioController.cs
+++ Controllers/FormularioController.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using Proyect_mvc.Models;
 
 namespace Proyect_mvc.Controllers
 {
     public class FormularioController : Controller
     {
-        public IActionResult Index()
+ 
+        public IActionResult Index(string TextBoxNombre, string TextBoxApellidos, string TextBoxDireccion, string TextBoxRequerimiento)
         {
-            return View();
+            var path = "/home/pickle/RiderProjects/Proyect-mvc/Models/Ciudades.txt";
+            string contenido = System.IO.File.ReadAllText(path);
+            string []ciudades = contenido.Split('\n');
+            var mi_formulario = new Formulario();
+            mi_formulario.Ciudad = new List<string>();
+            foreach(string line in ciudades)
+            {
+                (mi_formulario.Ciudad).Add(line);
+            }
+            mi_formulario.Nombres = TextBoxNombre;
+            mi_formulario.Apellidos = TextBoxApellidos;
+            mi_formulario.Direccion = TextBoxDireccion;
+            mi_formulario.Requerimiento = TextBoxRequerimiento;
+
+            return View(mi_formulario);
+
         }
+        
+        
+        /*public IActionResult Index(string TextBoxNombre)
+        {
+            var mi_form = new Formulario();
+            mi_form.Nombrecito = TextBoxNombre;
+            //return Content($"Nombre: {TextBoxNombre}");
+            return View(mi_form);
+        }*/
     }
 }
---
+++ Models/Formulario.cs
@@ -0,0 +1,17 @@
+
+namespace Proyect_mvc.Models
+{
+    public class Formulario
+    {
+        public List<string>? Ciudad { get; set; }
+        public string? Nombres {get; set; }
+        public string? Apellidos {get; set; }
+        public string? Direccion {get; set; }
+        public string? Requerimiento {get; set; }
+
+    }
+    public class Informacion
+    {
+        public string? Nombre {get; set; }
+    }
+}
