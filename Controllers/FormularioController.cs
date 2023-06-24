42734ce27c2c6e1cd7a6b09b23eee48a285c1c3a
Pickle <alonsochullunquia@gmail.com>
2023-06-24 13:42:04 -0500

    strings y ciudad selector hecho y mejorado

--- Controllers/FormularioController.cs
+++ Controllers/FormularioController.cs
@@ -5,34 +5,23 @@ namespace Proyect_mvc.Controllers
 {
     public class FormularioController : Controller
     {
- 
-        public IActionResult Index(string TextBoxNombre, string TextBoxApellidos, string TextBoxDireccion, string TextBoxRequerimiento)
-        {
-            var path = "/home/pickle/RiderProjects/Proyect-mvc/Models/Ciudades.txt";
-            string contenido = System.IO.File.ReadAllText(path);
-            string []ciudades = contenido.Split('\n');
-            var mi_formulario = new Formulario();
-            mi_formulario.Ciudad = new List<string>();
-            foreach(string line in ciudades)
-            {
-                (mi_formulario.Ciudad).Add(line);
-            }
+        private Formulario mi_formulario = new Formulario();
+        public IActionResult Index(){
+            return View(mi_formulario);
+        }
+        [HttpPost]
+        public IActionResult Procesame(string TextBoxNombre, 
+                                        string TextBoxApellidos, 
+                                        string TextBoxDireccion, 
+                                        string TextBoxRequerimiento,
+                                        string DropDownListCiudad
+        ) {
             mi_formulario.Nombres = TextBoxNombre;
             mi_formulario.Apellidos = TextBoxApellidos;
             mi_formulario.Direccion = TextBoxDireccion;
             mi_formulario.Requerimiento = TextBoxRequerimiento;
-
-            return View(mi_formulario);
-
+            ViewBag.CiudadS = DropDownListCiudad;
+            return View("Index",mi_formulario);
         }
-        
-        
-        /*public IActionResult Index(string TextBoxNombre)
-        {
-            var mi_form = new Formulario();
-            mi_form.Nombrecito = TextBoxNombre;
-            //return Content($"Nombre: {TextBoxNombre}");
-            return View(mi_form);
-        }*/
     }
 }
--- Models/Formulario.cs
+++ Models/Formulario.cs
@@ -3,11 +3,22 @@ namespace Proyect_mvc.Models
 {
     public class Formulario
     {
-        public List<string>? Ciudad { get; set; }
-        public string? Nombres {get; set; }
-        public string? Apellidos {get; set; }
-        public string? Direccion {get; set; }
-        public string? Requerimiento {get; set; }
+        public List<string> Ciudad { get; set; } = new List<string>();
+        public string Nombres {get; set; } = "";
+        public string Apellidos {get; set; } = "";
+        public string Direccion {get; set; } = "";
+        public string Requerimiento {get; set; } = "";
+        public string CiudadSeleccionada { get; set; } = "Lima";
+        
+        public Formulario(){
+            var path = "/home/pickle/RiderProjects/Proyect-mvc/Models/Ciudades.txt";
+            string contenido = System.IO.File.ReadAllText(path);
+            string []ciudades = contenido.Split('\n');
+            foreach(string line in ciudades)
+            {
+                Ciudad.Add(line);
+            }
+        }
 
     }
     public class Informacion
